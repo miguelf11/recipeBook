042acc4236dcce833499db3672d0245bbcc186a3
Miguel F <miguelachof11@gmail.com>
2017-01-28 19:57:33 -0400

    easy forms with autoform

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Intermediate curse of meteor
 
+https://www.youtube.com/watch?v=BI8IslJHSag&list=PLLnpHn493BHFYZUSK62aVycgcAouqBt7V
+
+
---
+++ collections/Recipes.js
@@ -0,0 +1,34 @@
+Recipes = new Meteor.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+	name: {
+		type: String,
+		label: "Name"
+	},
+	desc: {
+		type: String,
+		label: "Description"
+	},
+	author: {
+		type: String,
+		label: "Author",
+		autoValue: function(){
+			return this.userId
+		},
+		autoform: {
+			type: "hidden"
+		}
+	},
+	createdAt: {
+		type: Date,
+		label: "Create At",
+		autoValue: function(){
+			return new Date()
+		},
+		autoform: {
+			type: "hidden"
+		}
+	}
+});
+
+Recipes.attachSchema( RecipeSchema);
